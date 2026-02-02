6edb3b7412f880ddcd519bc0eaedbc3fdc397573
Timalsinarabin <timalsinarabin11@gmail.com>
2026-02-02 19:37:35 +0545

    chore: addes sw register

--- swregister.js
+++ swregister.js
@@ -4,7 +4,7 @@ const APP={
     init(){
         if('serviceWorker' in navigator){
             navigator.serviceWorker.register('./sw.js',{
-                scope: '/Note-app/'
+                scope: '/Inventory/'
             }).then(registration=>{
                 APP.SW = registration.installing || 
                         registration.waiting ||
@@ -14,40 +14,12 @@ const APP={
             navigator.serviceWorker.addEventListener('message',({data})=>{
                 console.log("message from service worker",data);
             });
-            const btn = document.querySelector('.installBtn');
-            if(btn) btn.style.display = 'none';
-
-            window.addEventListener('beforeinstallprompt',(ev)=>{
-                ev.preventDefault();
-                APP.deferredInstall = ev;
-                console.log("beforeinstallprompt");
-                if(btn) btn.style.display = 'block';
-            });
-
-            window.addEventListener('appinstalled', (ev) => {
-                console.log("app installed", ev);
-                APP.deferredInstall = null;
-                if(btn){ btn.style.display = 'none'; }
-            });
-
-            btn?.addEventListener('click',APP.startChromeInstall); 
         }
         else{
             console.log("service workers are not supported");
         }
         
     },
-    startChromeInstall(){
-            if(APP.deferredInstall){
-                console.log(APP.deferredInstall);
-                APP.deferredInstall.prompt();
-                APP.deferredInstall.userChoice.then((choice)=>{
-                    console.log("User choice:",choice.outcome);
-                    APP.deferredInstall = null;
-            }
-        );
-        }
-    }
 };
 
 document.addEventListener('DOMContentLoaded',APP.init);
